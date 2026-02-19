b77c427aae0dd7677e093dcdfc03c4145d1a04ac
Yichang Luo <129069372+YeeCharm@users.noreply.github.com>
2026-02-19 18:18:08 +0800

    Add Inria dataset configuration for segmentation

---
+++ src/open_vocabulary_segmentation/segmentation/configs/_base_/datasets/inria.py
@@ -0,0 +1,42 @@
+# ------------------------------------------------------------------------------
+# Inria Config
+# ------------------------------------------------------------------------------
+_base_ = ["../custom_import.py"]
+
+from mmseg.registry import DATASETS
+from mmseg.datasets import BaseSegDataset
+import mmcv
+
+@DATASETS.register_module(force=True)
+class InriaDataset(BaseSegDataset):
+    """Inria dataset for building extraction."""
+    METAINFO = dict(
+        classes=('background', 'building'),
+        palette=[[0, 0, 0], [255, 255, 255]]
+    )
+
+    def __init__(self, img_suffix='.png', seg_map_suffix='.png', **kwargs):
+        super().__init__(img_suffix=img_suffix, seg_map_suffix=seg_map_suffix, **kwargs)
+
+dataset_type = "InriaDataset"
+data_root = "/mnt/sdb/luoyichang/datasets/building_extraction/Inria/processed"
+
+test_pipeline = [
+    dict(type="LoadImageFromFile"),
+    dict(type="LoadAnnotations"),
+    dict(type="Resize", scale=(448, 448), keep_ratio=True),
+    dict(type='FloatImage'),
+    dict(type="PackSegInputs")
+]
+
+data = dict(
+    test=dict(
+        type=dataset_type,
+        data_root=data_root,
+        data_prefix=dict(img_path='img_dir/split_test', seg_map_path='ann_dir/split_test'),
+        pipeline=test_pipeline,
+        reduce_zero_label=False
+    )
+)
+
+test_cfg = dict(mode="slide", stride=(224, 224), crop_size=(448, 448))
