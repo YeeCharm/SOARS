1f7c3984f8c228732cbb62993bd374407607d18f
Yichang Luo <129069372+YeeCharm@users.noreply.github.com>
2026-02-19 18:17:11 +0800

    Add CWLD dataset configuration for segmentation

---
+++ src/open_vocabulary_segmentation/segmentation/configs/_base_/datasets/cwld.py
@@ -0,0 +1,42 @@
+# ------------------------------------------------------------------------------
+# CWLD Config
+# ------------------------------------------------------------------------------
+_base_ = ["../custom_import.py"]
+
+from mmseg.registry import DATASETS
+from mmseg.datasets import BaseSegDataset
+import mmcv
+
+@DATASETS.register_module(force=True)
+class CWLDDataset(BaseSegDataset):
+    """CWLD dataset for construction waste landfill detection."""
+    METAINFO = dict(
+        classes=('background', 'waste_landfill'),
+        palette=[[0, 0, 0], [255, 255, 255]]
+    )
+
+    def __init__(self, img_suffix='.png', seg_map_suffix='.png', **kwargs):
+        super().__init__(img_suffix=img_suffix, seg_map_suffix=seg_map_suffix, **kwargs)
+
+dataset_type = "CWLDDataset"
+data_root = "/mnt/sdb/luoyichang/datasets/waste_landfill/CWLD/processed"
+
+test_pipeline = [
+    dict(type="LoadImageFromFile"),
+    dict(type="LoadAnnotations"),
+    dict(type="Resize", scale=(448, 448), keep_ratio=True),
+    dict(type='FloatImage'),
+    dict(type="PackSegInputs")
+]
+
+data = dict(
+    test=dict(
+        type=dataset_type,
+        data_root=data_root,
+        data_prefix=dict(img_path='img_cvt', seg_map_path='label_cvt'),
+        pipeline=test_pipeline,
+        reduce_zero_label=False
+    )
+)
+
+test_cfg = dict(mode="slide", stride=(224, 224), crop_size=(448, 448))
