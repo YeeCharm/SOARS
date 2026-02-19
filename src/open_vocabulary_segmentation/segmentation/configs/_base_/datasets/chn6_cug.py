c439409cb88b05bd08ceaba8f7307d6bf8de8c50
Yichang Luo <129069372+YeeCharm@users.noreply.github.com>
2026-02-19 18:16:50 +0800

    Add CHN6-CUG dataset configuration for segmentation

---
+++ src/open_vocabulary_segmentation/segmentation/configs/_base_/datasets/chn6_cug.py
@@ -0,0 +1,42 @@
+# ------------------------------------------------------------------------------
+# CHN6-CUG Config
+# ------------------------------------------------------------------------------
+_base_ = ["../custom_import.py"]
+
+from mmseg.registry import DATASETS
+from mmseg.datasets import BaseSegDataset
+import mmcv
+
+@DATASETS.register_module(force=True)
+class CHN6CUGDataset(BaseSegDataset):
+    """CHN6-CUG dataset for road extraction."""
+    METAINFO = dict(
+        classes=('background', 'road'),
+        palette=[[0, 0, 0], [255, 255, 255]]
+    )
+
+    def __init__(self, img_suffix='.jpg', seg_map_suffix='.png', **kwargs):
+        super().__init__(img_suffix=img_suffix, seg_map_suffix=seg_map_suffix, **kwargs)
+
+dataset_type = "CHN6CUGDataset"
+data_root = "/mnt/sdb/luoyichang/datasets/road_extraction/CHN6-CUG/val"
+
+test_pipeline = [
+    dict(type="LoadImageFromFile"),
+    dict(type="LoadAnnotations"),
+    dict(type="Resize", scale=(448, 448), keep_ratio=True),
+    dict(type='FloatImage'),
+    dict(type="PackSegInputs")
+]
+
+data = dict(
+    test=dict(
+        type=dataset_type,
+        data_root=data_root,
+        data_prefix=dict(img_path='image_cvt', seg_map_path='label_cvt'),
+        pipeline=test_pipeline,
+        reduce_zero_label=False
+    )
+)
+
+test_cfg = dict(mode="slide", stride=(224, 224), crop_size=(448, 448))
